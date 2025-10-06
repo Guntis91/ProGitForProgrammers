44966046119c649d19fa412803d3b1368edf6cb2
Guntis S <2501993@uad.ac.uk>
2025-10-06 01:13:14 +0100

    changes made in visual studio

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -9,7 +9,7 @@ namespace ConsolePractice
             Console.WriteLine("Hello, World!");
             Console.WriteLine("New Line of Code");
             Console.WriteLine("Hello, World again!");
-            Console.WriteLine("Hello, this will be last line of the code!!");
+            Console.WriteLine("Hello, this will be last line of the code!!!");
 
         }
     }
